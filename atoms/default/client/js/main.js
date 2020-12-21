a6abe1ec6b30632201dd670abc057d8671d40943
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-12-21 14:19:08 +0100

    adds a back arrow

--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -3,20 +3,29 @@ el.src = '<%= atomPath %>/app.js';
 document.body.appendChild(el);
 
 
+let frontlineNav = document.querySelector('.frontline-nav');
+if (frontlineNav) {
+  console.log('has-nav');
+  const articleEl = document.querySelector('#article');
+  const headerEl = document.querySelector('.content__head');
+  console.log(headerEl, articleEl)
+  articleEl.insertBefore(frontlineNav, headerEl);
+}
+
 
 setTimeout(() => {
-  if (window.resize) {  
+  if (window.resize) {
     const html = document.querySelector('html')
     const body = document.querySelector('body')
-  
+
     html.style.overflow = 'hidden'
     html.style.margin = '0px'
     html.style.padding = '0px'
-  
+
     body.style.overflow = 'hidden'
     body.style.margin = '0px'
     body.style.padding = '0px'
-  
-  window.resize()
+
+    window.resize()
   }
-},100)
+}, 100)
